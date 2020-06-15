3326fc637152fa907ebe3921c6624499dbe0c64d
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2020-06-15 13:02:17 +0200

    neo4j integration

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/neo4j/neo4j-go-driver/neo4j"
 )
 
 func main() {
@@ -12,18 +14,44 @@ func main() {
 		os.Exit(1)
 	}
 
+	driver, session, connErr := connectToNeo4j()
+
+	if connErr != nil {
+		fmt.Println("Error connecting to Database:", connErr)
+		os.Exit(1)
+	}
+
+	defer driver.Close()
+
+	defer session.Close()
+
 	url := os.Args[1]
 	ev := make(chan link)
 	r := retriever{visited: make(map[string]bool)}
 	r.addEvent("newLink", ev)
 
-	go func() {
+	go func(session *neo4j.Session) {
 		for {
 			l := <-ev
 			fmt.Println(l.source + " -> " + l.target)
+			_, err := createNode(session, &l)
+
+			if err != nil {
+				fmt.Println("Failed to create node:", err)
+			}
+
 		}
-	}()
+	}(&session)
 
 	r.crawl(url)
+
+	fmt.Println("Creation of relationship between nodes.. ")
+	_, qErr := createNodesRelationship(&session)
+
+	if qErr == nil {
+		fmt.Println("Nodes updated")
+	} else {
+		fmt.Println("Error while updating nodes:", qErr)
+	}
 
 }
---
+++ neo4j.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+func connectToNeo4j() (neo4j.Driver, neo4j.Session, error) {
+
+	configForNeo4j40 := func(conf *neo4j.Config) { conf.Encrypted = false }
+
+	driver, err := neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth(
+		"neo4j", "alice!in!wonderland", ""), configForNeo4j40)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	sessionConfig := neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite}
+	session, err := driver.NewSession(sessionConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return driver, session, nil
+}
+
+func createNode(session *neo4j.Session, l *link) (neo4j.Result, error) {
+	r, err := (*session).Run("CREATE (:WebLink{source: $source, target: $target}) ", map[string]interface{}{
+		"source": l.source,
+		"target": l.target,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return r, err
+}
+
+func createNodesRelationship(session *neo4j.Session) (neo4j.Result, error) {
+	r, err := (*session).Run("MATCH (a:WebLink),(b:WebLink) WHERE a.target = b.source CREATE (a)-[r:point_to]->(b)", map[string]interface{}{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return r, err
+}

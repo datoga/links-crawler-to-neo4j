4363a0d85210918d3f45b4f24f2d69719db9939b
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2020-06-15 12:31:34 +0200

    Retrieving a single page from the internet

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type responseWriter struct{}
 
 func main() {
-	fmt.Println("Hello world!")
+
+	resp, err := http.Get("http://www.sfeir.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	rw := responseWriter{}
+	io.Copy(rw, resp.Body)
+}
+
+func (responseWriter) Write(bs []byte) (int, error) {
+	fmt.Printf(string(bs))
+
+	return len(bs), nil
 }
